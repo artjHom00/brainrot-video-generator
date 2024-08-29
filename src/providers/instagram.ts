e6cad965f0ad246cc3694d8b7a039d10950792a8
artyom <agasparyan0000@gmail.com>
2024-08-29 19:36:44 +0400

    instagram class finished. now able to scrape from user

--- src/providers/instagram.ts
+++ src/providers/instagram.ts
@@ -1,22 +1,26 @@
 import puppeteer, { Browser, Page, PuppeteerLaunchOptions } from 'puppeteer';
+import { Post } from './types/post.interface'
+import axios from 'axios';
 
 class InstagramScraper {
-    browser!: Browser; // Use definite assignment assertion
-    page!: Page;
-    HOST: string;
+    protected browser!: Browser; // Use definite assignment assertion
+    protected page!: Page;
+    private puppeteerLaunchOptions?: PuppeteerLaunchOptions;
+    private initialized: boolean;
+    private HOST: string;
 
     constructor(puppeteerLaunchOptions?: PuppeteerLaunchOptions) {
+        this.puppeteerLaunchOptions = puppeteerLaunchOptions
+        this.initialized = false;
         this.HOST = 'https://instagram.com'
-        // Initialize browser asynchronously
-        this.initialize(puppeteerLaunchOptions).catch((error) => {
-            console.error('Error initializing InstagramScraper:', error);
-        });
     }
 
-    private async initialize(puppeteerLaunchOptions?: PuppeteerLaunchOptions): Promise<void> {
+    protected async initialize(puppeteerLaunchOptions?: PuppeteerLaunchOptions): Promise<void> {
         try {
             this.browser = await puppeteer.launch(puppeteerLaunchOptions);
             this.page = await this.browser.newPage();
+
+            this.initialized = true;
         } catch (e) {
             console.error('initialize -> catch:', e);
             throw e; // Rethrow error after logging it
@@ -29,21 +33,48 @@ class InstagramScraper {
 
     private async goToUserPage(userId: string): Promise<void> {
         try {
-            await this.page.goto(`${this.HOST}/${userId}/reels/`);
-            
-            await this.page.waitForNetworkIdle();
-
-            return;
+            if(this.page) {
+                await this.page.goto(`${this.HOST}/${userId}/reels/`);
+                
+                await this.page.waitForNetworkIdle();
+    
+                return;
+            }
         } catch (e) {
             console.log('goToUserPage -> catch', e);
             throw e;
         }
     }
 
-    protected async scrapeUsersPosts(userId: string): Promise<void> {
+    public async scrapeUsersPosts(userId: string): Promise<Post[]> {
         try {
+
+            // Initialize browser asynchronously
+            if(!this.initialized) {
+                await this.initialize(this.puppeteerLaunchOptions)
+            }
             
             await this.goToUserPage(userId);
+
+            // getting posts
+            let postsIds: string[] = await this.page.evaluate(() => {
+                const allPostsDOM = document.querySelectorAll('._al5p');
+                const postsIds = [];
+                
+                for(let post of allPostsDOM) {
+                    let postId = post.children[0].children[0].getAttribute('href')?.split('/')[2];
+
+                    if(postId) {
+                        postsIds.push(postId)
+                    }
+                }
+
+                return postsIds;
+            })
+
+            let fullPostsData: Post[] = await Promise.all(postsIds.map((postId) => this.downloadPost(postId)))
+
+            return fullPostsData
 
         } catch (e) {
             console.log('scrapeUsersPosts -> catch', e);
@@ -51,8 +82,21 @@ class InstagramScraper {
         }
     }
 
-    private async downloadPost(postId: string): Promise<void> {
+    private async downloadPost(postId: string): Promise<Post> {
         try {
+            
+            const requestUrl: string = this.getInstagramPostJSONUrl(postId);
+
+            const { data: { graphql: { shortcode_media: postData } } } = await axios.get(requestUrl)
+
+            return {
+                postId,
+                thumbnailUrl: postData.display_url,
+                videoUrl: postData.video_url,
+                dimensions: postData.dimensions,
+                location: postData.location,
+                video_url: postData.video_url
+            }
             
         } catch (e) {
             console.log('downloadPost -> catch', e);
@@ -61,7 +105,7 @@ class InstagramScraper {
     }
 
     // Add a method to close the browser
-    async close(): Promise<void> {
+    protected async close(): Promise<void> {
         if (this.browser) {
             await this.browser.close();
         }
---
+++ src/providers/types/post.interface.ts
@@ -0,0 +1,11 @@
+export interface Post {
+    postId: string;
+    thumbnailUrl: string;
+    videoUrl: string;
+    dimensions: {
+        width: string;
+        height: string;
+    }
+    location: string;
+    video_url: string;
+}
